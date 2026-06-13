612a7240a5ea27ecd138afb893163bb8c22d4ee1
sdssobo06-commits <sdssobo06@gmail.com>
2026-06-13 13:32:06 +0500

    hader o'zgardu

--- frontend/src/shared/components/Header.jsx
+++ frontend/src/shared/components/Header.jsx
@@ -25,7 +25,7 @@ const Header = () => {
           <div className="logo-glow-wrapper">
             <ShoppingBag className="logo-icon" />
           </div>
-          <span className="logo-text">Distinction</span>
+          <span className="logo-text">Ms Saodat</span>
         </div>
 
         {/* Center: Navigation Dock */}
